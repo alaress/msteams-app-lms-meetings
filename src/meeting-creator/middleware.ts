06363f1a58b7cf85e2f05cfccf8284a11b804149
Steve Chan <steve.chan@alaress.com.au>
2020-03-31 12:02:24 +1100

    Alter final screen's behaviour [SA-10194]
    This changes the default Microsoft Teams app to redirect to /api/teams.php with two querystring params: the schoolbox domain and the meeting url. This allows us to return the meetingUrl's value to our own domain, and therefore inspect iframe contents to extract it and send it to the cke dialog.

--- src/meeting-creator/middleware.ts
+++ src/meeting-creator/middleware.ts
@@ -4,27 +4,49 @@ import { createMeetingService } from './service';
 import { push } from 'connected-react-router';
 
 export function createMeetingMiddleware(): Middleware {
-  const service = createMeetingService();
+    const service = createMeetingService();
 
-  return store => next => action => {
-    if (action.type === CREATE_MEETING_COMMAND) {
-      service
-        .createMeeting(action.meeting)
-        .then(meeting => {
-          store.dispatch({
-            type: MEETING_CREATED_EVENT,
-            meeting
-          });
-        })
-        .catch(error => {
-          console.error('Create meeting failed: ', error);
-          store.dispatch(push('/error'));
-        });
-    }
+    return store => next => action => {
+        if (action.type === CREATE_MEETING_COMMAND) {
+            service
+                .createMeeting(action.meeting)
+                .then(meeting => {
+                    store.dispatch({
+                        type: MEETING_CREATED_EVENT,
+                        meeting
+                    });
+                })
+                .catch(error => {
+                    console.error('Create meeting failed: ', error);
+                    store.dispatch(push('/error'));
+                });
+        }
 
-    if (action.type === MEETING_CREATED_EVENT) {
-      store.dispatch(push('/copyMeeting'));
-    }
-    next(action);
-  };
+        if (action.type === MEETING_CREATED_EVENT) {
+
+            /* We store a schoolbox instance's domain as a url param
+             * We use it to redirect to another location in that domain where
+             * we can dump out the meeting's url
+             */
+            const url = new URL(document.location.href);
+            let returnUrl = new URL(url.searchParams.get('instance') + '/api/teams.php');
+
+            /* The meeting's url is also another querystring param
+             * These two pieces of the puzzle are then visited and echo'd
+             * so that we can pass the message up the schoolbox instance's iframe hierarchy
+             */
+            let returnUrlSearchParams = returnUrl.searchParams;
+            returnUrlSearchParams.set('meetingUrl', action.meeting.joinWebUrl);
+
+            returnUrl.search = returnUrlSearchParams.toString();
+
+            document.location.href = returnUrl.toString();
+
+            /* What is dead may never die
+             * (This is what was previously here)
+             */
+            // store.dispatch(push('/copyMeeting'));
+        }
+        next(action);
+    };
 }
